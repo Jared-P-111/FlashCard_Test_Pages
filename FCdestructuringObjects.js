9301fbcab03a4ea4dac63294fe5a0546cc15ac1c
Jared_P <jaredpisell@gmail.com>
2021-02-01 16:40:37 -0800

    Revamped the entire Flash Card

--- FCdestructuringObjects.js
+++ FCdestructuringObjects.js
@@ -1,46 +1,53 @@
 'use-strict';
 //------------------------------DESTRUCTURING OBJECTS--------------------------------
-console.log('-----------------------Problem 1------------------------------');
 //------------------------SELECTING PROPERTY'S IN OBJECTS----------------------------
 
-//Create an object named animalsAtZoo with the following criteria.
-//      1.) Make two property's named cage1 & cage2 with a different animal in each.
-//      2.) Make two property's that are Arrays named cage3 and cage 4 with 3 animals in each. In cage
-//          3 put chimp<name> etc... and in cage 4 put baboon<name> etc...
+//Using the below Object solve the destructuring problems.
 
 const animalsAtZoo = {
-  cage1: 'bear',
-  cage2: 'tiger',
-  cage3: ['chimpAria', 'chimpGeorge', 'chimpBob'],
-  cage4: ['baboonSteve', 'babboonMike', 'babboonSarah'],
+  cage1: 'Tony',
+  cage2: 'Billy',
+  cage3: 'Philly',
+  cage4: 'Brian',
+  animalCage: ['Baboon Steve', 'Baboon Mike', 'Baboon Sarah', 'Tony the Tiger', 'Billy the Bear'],
+  foodMenuMeat: ['chicken', 'lamb', 'beef', 'turkey'],
+  foodMenuVegtables: ['broccoli', 'asparagus', 'carrots'],
 };
-console.log(animalsAtZoo);
 
-console.log('-----------------------Problem 2------------------------------');
+console.log('-----------------------Problem 1------------------------------');
 //------------------------ASSIGN VARIABLES WITH DESTRUCTURING----------------------------
 
-//Create three variables using destructuring use the names in the console log.
+//Create three variables using destructuring use the names in the console log. Then uncomment to use.
 
-const { cage1, cage3, cage4 } = animalsAtZoo;
-console.log(cage1, cage3, cage4);
+//console.log(cage1, cage3, cage4);
 
-console.log('-----------------------Problem 3------------------------------');
+console.log('-----------------------Problem 2------------------------------');
 //------------------------RENAMING VARIABLES WITH DESTRUCTURING----------------------
 
-//Rename cage1, cage3, cage4 to bearCage, chimpCage, baboonCage. Use destructuring.
-const { cage1: bearCage, cage3: chimpCage, cage4: baboonCage } = animalsAtZoo;
-console.log(bearCage, chimpCage, baboonCage);
+//Rename cage1, cage3, cage4 to bearCage, chimpCage, baboonCage. Use destructuring. Then uncomment to use.
 
-console.log('-----------------------Problem 4------------------------------');
+//console.log(bearCage, chimpCage, baboonCage);
+
+console.log('-----------------------Problem 3------------------------------');
 //--------------MUTATING VARIABLES WITH AN OBJECT AND DESTRUCTURING----------------------
 
-//create two variables using let and named a and b and assign them numbers. Then create an object
-//called obj with 3 propertys each property key should a, b, c and the values assign them numbers.
-//Now using the global var's mutate them with the object.
+//create two variables using let named a and b and assign them numbers. Then create an object
+//called obj with 3 propertys each property key should be a, b, c and then assign values with numbers.
+//Now using the outside scoped var's mutate them with the object. Use the below console .log
+//Uncomment them when ready to use.
 
-let a = 100;
-let b = 200;
+//console.log(a, b);
 
-const obj = { a: 22, b: 33, c: 44 };
-({ a, b } = obj);
-console.log(a, b);
+console.log('-----------------------Problem 4------------------------------');
+//----------------USING AN OBJECT AS AN ARGUMENT WITH DESTRUCTURING----------------------
+//animalIndex, meatIndex, vegIndex
+//Use the method below, inside the animalsAtZoo object that takes an object as the
+//argument the argument names are above. Then use that method to output new values using
+// the key names above as the arguments in the method. Hint do not forget to destructure
+
+/*
+animalFoodOrder: function () {
+    console.log(`Food order recieved: ${} & ${} will be deliverd to
+${}.`);
+  },
+*/
